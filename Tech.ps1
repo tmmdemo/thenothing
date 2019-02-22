2a47f3e869e560ea8456c6a048d2690f399c20c6
tmmdemo <37916298+tmmdemo@users.noreply.github.com>
2019-02-22 12:15:59 -0600

    Update Tech.ps1

--- Tech.ps1
+++ Tech.ps1
@@ -1,8 +1,10 @@
 function Invoke-Tech
 {
-	Write-Host "You've been pwned! v3";
+	Write-Host "You've been pwned! v4";
 	Start-Sleep 1;
 	netstat -an;
+	Start-Process "cmd.exe" '/c netstat -an;'
 	Start-Sleep 1;
 	tasklist;
+	Start-Process "cmd.exe" '/c tasklist;'
 }
